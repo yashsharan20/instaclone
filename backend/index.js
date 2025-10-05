1dc6a9a6b0bf0088f93303038ae21003551f6f5b
yashsharan20 <yashsharan2013@gmail.com>
2025-10-05 14:16:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use("/api/v1/post",postRoute);
 app.use("/api/v1/message",messageRoute);
 
 app.use(express.static(path.join(__dirname,"/frontend/dist")));
-app.get("*",(_,res)=>{
+app.get("/*",(_,res)=>{
     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
 });
 
