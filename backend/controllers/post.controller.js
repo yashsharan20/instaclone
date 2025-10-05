1cbacd5671214a58ab154e96cdbcfe58d92025bd
yashsharan20 <yashsharan2013@gmail.com>
2025-10-05 14:32:53 +0530

    Update post.controller.js

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -153,4 +153,96 @@ export const dislikePost = async (req, res) => {
             }
         }
 
-        return
+             return res.status(200).json({message:"Post disliked",success:true});
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const addComment = async(req,res)=>{
+    try {
+        const postId = req.params.id 
+        const commentKrneWalaUserKiId = req.id 
+        const {text} = req.body 
+        const post = await Post.findById(postId);
+        if(!text) return res.status(400).json({message:"Text is required"});
+        
+        const comment = await Comment.create({
+            text,
+            author:commentKrneWalaUserKiId,
+            post:postId
+        });
+        await comment.populate({
+            path:'author',
+            select:'username profilePicture'
+        });
+         post.comments.push(comment._id);
+         await post.save();
+         return res.status(201).json({
+            message:"Comment added successfully",
+            success:true,
+            comment
+         });
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const getCommentofPost = async(req,res)=>{
+    try {
+        const postId = req.params.id 
+        const comments = await Comment.find({post:postId}).sort({createdAt:-1})
+        .populate('author','username profilePicture');
+        if(!comments) return res.status(404).json({message:"No comments found for this post",succes:false});
+        return res.status(200).json({
+            comments,
+            success:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const deletePost = async(req,res)=>{
+    try {
+        const postId = req.params.id 
+        const authorId = req.id 
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({message:"Post not found",success:false});
+        if(post.author.toString() != authorId) return res.status(403).json({message:"Unauthorized",success:false});
+        await Post.findByIdAndDelete(postId);
+        let user = await User.findById(authorId);
+        user.posts = user.posts.filter(id=>id.toString() !== postId);
+        await user.save();
+        await Comment.deleteMany({post:postId});
+        return res.status(200).json({
+            message:"Post deleted successfully",
+            success:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const bookmarkPost = async(req,res)=>{
+    try {
+        const postId = req.params.id 
+        const authorId = req.id
+        const post = await Post.findById(postId); 
+        if(!post) return res.status(404).json({message:"Post not found",succes:false});
+        const user = await User.findById(authorId);
+        if(user.bookmarks.includes(postId)){
+            await user.updateOne({$pull:{bookmarks:postId}});
+            return res.status(200).json({
+                type:'unsaved',
+                message:"Post removed from bookmark",
+                success:true
+            });
+        }else{
+            await user.updateOne({$addToSet:{bookmarks:postId}});
+            return res.status(200).json({
+                type:'saved',
+                message:"Post bookmarked",
+                success:true
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
